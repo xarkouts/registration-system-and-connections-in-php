b801b58a9c520ef2c3669077858a2d587680d060
xarouts <74020157+xarouts@users.noreply.github.com>
2021-05-16 22:33:41 +0300

    Add files via upload

---
+++ ka.php
@@ -0,0 +1,30 @@
+<html>
+<?php  session_start();  ?>
+<head>
+<title>
+kalothorises 
+</title>
+</head>
+<body>
+<h1> ΚΑΛΟΣΟΡΗΣΕΣ <?php
+
+if(isset($_SESSION["olo"])){
+ print("$_SESSION[olo]");
+}elseif($_SESSION["olo"]==null){
+
+    header("Location:login.php");
+        exit();
+}else{
+ 
+
+    header("Location:login.php");
+    exit();
+
+}
+
+  ?>  </h1>
+
+
+<body>
+
+</html>
